f8f46152c71a86e473501c844022bba6a7a22807
j4nsuh <szeryfchudy666@gmailcom>
2023-10-10 19:01:52 +0200

    ugly but working

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -5,14 +5,35 @@
 //  Created by student on 10/10/2023.
 //
 
+import Foundation
 import SwiftUI
 
 struct CardView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var isFlipped: Bool = false
+    var content: String
+    
+    var body: some View{
+        Group{
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.white)
+                    .frame(minWidth: 120, minHeight: 50)
+                    .overlay(
+                        Text(content).font(.largeTitle)
+                    )
+                    .overlay(RoundedRectangle(
+                        cornerRadius: 12
+                    ).stroke(.blue, lineWidth: 2)
+                    ).overlay( RoundedRectangle(cornerRadius: 12).fill(.blue).opacity(isFlipped ? 0 : 1)
+                    )
+        }.onTapGesture{
+            isFlipped.toggle()
+        }
     }
+    
 }
 
+
+
 #Preview {
-    CardView()
+    CardView(content: "🐱")
 }
--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,16 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cardContents = ["🤙", "👅", "🍑", "🍌", "🐱" ,"🐵"]
+    @State private var displayedCards = ["🤙", "👅"]
+    
+    func addCards(){
+        let currentIndex = displayedCards.count
+        if currentIndex < cardContents.count{
+            let endIndex = min(currentIndex+2, cardContents.count)
+            displayedCards.append(contentsOf: cardContents[currentIndex..<endIndex])
+        }
+       
+    }
+    
+    func removeCards(){
+        if displayedCards.count > 2{
+            displayedCards.removeLast(2)
+        }
+    }
+    
+    func adjustCardNumber(by offset: Int)-> some View{
+        if offset > 0{
+            addCards()
+        }else if offset < 0{
+            removeCards()
+        }
+        return EmptyView()
+    }
+    
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            cardDisplay
+            HStack{
+                Button(action: {
+                    adjustCardNumber(by: -2)
+                }){
+                    Image(systemName: "minus.circle.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20).padding()
+                }.disabled(displayedCards.count == 2).border(.blue)
+                Spacer()
+                Button(action: {
+                    adjustCardNumber(by: 2)
+                }){
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 20, height: 20).padding()
+                }.disabled(displayedCards.count == cardContents.count).border(.blue)
+            }.padding()
         }
-        .padding()
+        
+        
+        
+    }
+    var cardDisplay: some View{
+        ScrollView{
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))], content: {
+                ForEach(displayedCards, id: \.self){
+                    content in
+                    CardView(content: content)
+                }
+            }).padding()
+        }
     }
 }
+
+
 
 #Preview {
     ContentView()
